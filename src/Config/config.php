5b8450a4f288e89ac0492a883e061c81b4a04e39
GP247 <gp247.net@gmail.com>
2025-02-20 05:28:49 +0700

    Remove variable route

--- src/Config/config.php
+++ src/Config/config.php
@@ -7,9 +7,6 @@ return [
             2 => 'localization',
         ],
         'route' => [
-            //Prefix member, as domain.com/customer/login
-            'GP247_PREFIX_MEMBER' => env('GP247_PREFIX_MEMBER', 'customer'), 
-    
             //Prefix lange on url, as domain.com/en/abc.html
             //If value is empty, it will not be displayed, as dommain.com/abc.html
             'GP247_SEO_LANG' => env('GP247_SEO_LANG', 0),
