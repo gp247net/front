41a0d0f404c3e1a829a3bd5e90f46380b3770581
gp247 <gp247.net@gmail.com>
2025-07-30 00:41:13 +0700

    Improve code

--- src/Config/config.php
+++ src/Config/config.php
@@ -23,7 +23,6 @@ return [
             'front_home' => 'admin.layout_block_page.home',
             'front_contact' => 'admin.layout_block_page.contact',
             'front_about' => 'admin.layout_block_page.about',
-            'front_page_list' => 'admin.layout_block_page.page_list',
             'front_page_detail' => 'admin.layout_block_page.page_detail',
             'front_news_list' => 'admin.layout_block_page.news_list',
             'front_news_detail' => 'admin.layout_block_page.news_detail',
